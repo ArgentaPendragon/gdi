08a8459c18fb875422df7bfc92f26c6f1340c871
lunarcwhite <rizkym058@gmail.com>
2025-04-21 20:58:45 +0700

    tes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+  return (
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-mono text-white">
+        Hello world! p
+        Welcome to my React app!
+      </h1>
+    </div>
+  )
+}
+
+export default App
